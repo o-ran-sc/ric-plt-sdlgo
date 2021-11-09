0192be751646808688ec40e8663f48799bf7b324
Petri Ovaska <petri.ovaska@nokia.com>
2021-11-09 08:28:48 +0200

    Implement 'get' and 'get keys' -SDL CLI commands
    sdlcli get -command is used to display one or many resources.
    
     Usage:
       sdlcli get [command]
    
    sdlcli get keys -subcommand is used to list keys in the given
    namespace matching key search pattern.
    
     Usage:
       sdlcli get keys <namespace> [pattern|default '*'] [flags]
    
    Issue-Id: RIC-113
    Change-Id: I86b81ff8d8d2cdc0e959e285b19f93696ebe377e
    Signed-off-by: Petri Ovaska <petri.ovaska@nokia.com>

--- internal/cli/cli_private_fn_test.go
+++ internal/cli/cli_private_fn_test.go
@@ -26,6 +26,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	NewRootCmd = newRootCmd
+	NewHealthCheckCmd = newHealthCheckCmd
+	NewKeysCmdForTest = newKeysCmd
+	NewGetCmdForTest = newGetCmd
+)
+
 // NewHealthCheckCmdForTest is used only in unit tests to mock database.
 func NewHealthCheckCmdForTest(dbCreateCb DbCreateCb) *cobra.Command {
 	return newHealthCheckCmd(dbCreateCb)
